03e27fe6a3e4aea4863e036306725703a08caa3b
Débora <deboracristinapereira123@gmail.com>
2020-10-19 13:17:42 -0300

    Layout MainActivity

--- Movie/app/src/main/java/com/navita/movie/MainActivity.java
+++ Movie/app/src/main/java/com/navita/movie/MainActivity.java
@@ -3,12 +3,17 @@ package com.navita.movie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listMovies = (ListView) findViewById(R.id.list_movies);
     }
 }
