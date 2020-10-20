e6c0bd0ef1d573705bb1065aaf31d7add658a49c
Débora <deboracristinapereira123@gmail.com>
2020-10-20 02:50:07 -0300

    Popular ListView - incompleto

--- Movie/app/src/main/java/com/navita/movie/MainActivity.java
+++ Movie/app/src/main/java/com/navita/movie/MainActivity.java
@@ -3,8 +3,22 @@ package com.navita.movie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +31,68 @@ public class MainActivity extends AppCompatActivity {
 
         listMovies = (ListView) findViewById(R.id.list_movies);
 
-        String[] movies = {"Atleta A", "Filme B", "Filme C", "Filme D", "Filme E", "Filme F", "Filme G", "Filme H",
-        "Filme I", "Filme J", "Filme K", "Filme M", "Filme N", "Filme O", "Filme P", "Filme Q", "Filme S"};
+        ArrayList<String> movieTitle = new ArrayList<String>();
+
+        // Instanciar requestQueue
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=91ccbf054dea918f20216e44a5996892";
+
+        // Solicitar resposta (get) do backend em String
+
+        // Listener implementa método onResponse e onError (assíncrono)
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                // Transformar string em JSON
+
+                JSONObject jsonResponse = null;
+
+                try {
+                    jsonResponse = new JSONObject(response);
+
+                    // Array results, percorrer objetos e propriedade title
+
+                    JSONArray jsonArrayResults = jsonResponse.getJSONArray("results");
+
+                    for (int i = 0; i < jsonArrayResults.length(); i++) {
+
+                        JSONObject dataMovie = jsonArrayResults.getJSONObject(i);
+
+                        String title = dataMovie.getString("title");
+
+                        Log.d("=======titulo", title);
+
+                        // Adicionar título ao array de strings
+
+                       //etMovieList(title);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", ""+error);
+            }
+        });
+
+        // Adicionar request ao requestQueue
+
+        requestQueue.add(stringRequest);
+
+        final ArrayList<String> movies = new ArrayList<>();
+
+        for (int i = 0; i < movieTitle.size(); i++) {
+            movies.add(movieTitle.get(i));
+        }
+
+        Log.d("=======movieTitle", ""+movieTitle);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, movies);
         listMovies.setAdapter(arrayAdapter);
