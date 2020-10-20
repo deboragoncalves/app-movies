bbe291a5a207e09d33c6ab2a38c2542c1327b00e
Débora <deboracristinapereira123@gmail.com>
2020-10-19 21:13:47 -0300

    ListView estatica

--- Movie/app/src/main/java/com/navita/movie/MainActivity.java
+++ Movie/app/src/main/java/com/navita/movie/MainActivity.java
@@ -3,6 +3,7 @@ package com.navita.movie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listMovies = (ListView) findViewById(R.id.list_movies);
+
+        String[] movies = {"Atleta A", "Filme B", "Filme C", "Filme D", "Filme E", "Filme F", "Filme G", "Filme H",
+        "Filme I", "Filme J", "Filme K", "Filme M", "Filme N", "Filme O", "Filme P", "Filme Q", "Filme S"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, movies);
+        listMovies.setAdapter(arrayAdapter);
     }
 }
