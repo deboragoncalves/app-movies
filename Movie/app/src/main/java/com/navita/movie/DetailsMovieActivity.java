fd9150dff21dd8b80d0c2f936b02ee3850244876
Débora <deboracristinapereira123@gmail.com>
2020-10-22 16:52:06 -0300

    Detalhes e splash activity

--- Movie/app/src/main/java/com/navita/movie/DetailsMovieActivity.java
+++ Movie/app/src/main/java/com/navita/movie/DetailsMovieActivity.java
@@ -3,20 +3,100 @@ package com.navita.movie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
 
 public class DetailsMovieActivity extends AppCompatActivity {
+
+    private TextView movieTitle;
+    private TextView moviePopularity;
+    private TextView movieGenre;
+    private Button buttonBackList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_movie);
 
+        movieTitle = (TextView) findViewById(R.id.title_movie);
+        moviePopularity = (TextView) findViewById(R.id.popularity_movie);
+        movieGenre = (TextView) findViewById(R.id.genres_movie);
+        buttonBackList = (Button) findViewById(R.id.button_back_list);
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
-        int id = bundle.getInt("id");
-        Log.d("=====id", ""+id);
+        assert bundle != null;
+        int movieId = bundle.getInt("id");
+        requestDetailsMovie(movieId);
+
+        buttonBackList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void requestDetailsMovie(int movieId) {
+        String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=91ccbf054dea918f20216e44a5996892&language=pt-PT";
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                JSONObject movieDetails = null;
+
+                try {
+                    movieDetails = new JSONObject(response);
+
+                    JSONArray arrayGenres = movieDetails.getJSONArray("genres");
+
+                    for (int i = 0; i < arrayGenres.length(); i++) {
+                        JSONObject dataGenres = arrayGenres.getJSONObject(i);
+
+                        movieGenre.append(" " + dataGenres.getString("name") + ".");
+                    }
+
+                    String popularityMovie = String.valueOf(movieDetails.getInt("popularity"));
+                    String popularity = "Popularidade: " + popularityMovie + " mil";
+                    moviePopularity.setText(popularity);
+
+                    String title = "Título Original: " + movieDetails.getString("original_title");
+                    movieTitle.setText(title);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(stringRequest);
+
     }
 }
---
+++ Movie/app/src/main/java/com/navita/movie/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.navita.movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+
+        // Handler para começar a Splash e terminar depois de um certo tempo, chamando a main através da intent
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
