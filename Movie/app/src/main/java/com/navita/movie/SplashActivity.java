4488d5ee441635ce3f34a4e2d8227db09bf7db8b
Débora <deboracristinapereira123@gmail.com>
2020-10-23 01:06:31 -0300

    Direcionar para as activities na slash

--- Movie/app/src/main/java/com/navita/movie/SplashActivity.java
+++ Movie/app/src/main/java/com/navita/movie/SplashActivity.java
@@ -3,8 +3,10 @@ package com.navita.movie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,14 +15,31 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_activity);
 
-        // Handler para começar a Splash e terminar depois de um certo tempo, chamando a login através da intent
+        // Handler para começar a Splash e terminar depois de um certo tempo, chamando a activity da intent
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+
+                // Se já tiver logado, ir para main. Se não, login
+
+                SharedPreferences sharedPreferences = getSharedPreferences("data_login", MODE_PRIVATE);
+
+                Intent intent;
+
+                if (!sharedPreferences.getAll().isEmpty()) {
+
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+
+                    intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    
+                }
             }
         }, 4000);
     }
